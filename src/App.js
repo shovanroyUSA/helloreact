f1dfeb8e3f6592b5ccf3dcf7dd92588e3fcc5ce0
Shovan Roy <sroy1401@student.sfbu.edu>
2023-11-30 17:53:18 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+//import { Head } from './component/Head';
+import StylingAssignment from './component/StylingAssignment';
+
+function App() {
+  return (
+    
+    <div className="App">
+     
+      <StylingAssignment />
+   
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Head/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
